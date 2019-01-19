9725a05150b880fc92d58d9003c8c18f1e6e7508
Eric Crosson <eric.s.crosson@utexas.com>
2019-01-19 17:02:23 -0600

    Import Record as dep and use default export

--- src/random-record.ts
+++ src/random-record.ts
@@ -3,44 +3,14 @@
  * Generate a random talib-record
  */
 
-import randomInt = require('random-int')
+import randomInt from 'random-int'
+import Record from 'timeseries-record'
 
-
-/**
- * Copied definition of Record from package `talib-binding`.
- */
-export declare interface Record {
-    /**
-     * Time that the current Record began, in units of milliseconds
-     * elapsed since January 1, 1970.
-     */
-    Time: number,
-    /**
-     * Opening value of current Record.
-     */
-    Open: number,
-    /**
-     * Highest value observed during current Record.
-     */
-    High: number,
-    /**
-     * Lowest value observed during current Record.
-     */
-    Low: number,
-    /**
-     * Closing value of current Record.
-     */
-    Close: number,
-    /**
-     * Volume observed in current Record.
-     */
-    Volume: number
-}
 
 /**
  * Generate a random (and valid) Record.
  */
-export function randomRecord(): Record {
+export default function randomRecord(): Record {
 
     let open = randomInt(100)
     let high = randomInt(open, open + 100)
--- test/test-random-record.ts
+++ test/test-random-record.ts
@@ -5,16 +5,15 @@ const expect = chai.expect
  * Library under test
  */
 
-import { Record, randomRecord } from '../src/random-record'
+import Record from 'timeseries-record'
+import randomRecord from '../src/random-record'
 
 
 describe('#randomRecord', () => {
 
-    let record: Record
-
     it('should generate a valid record', () => {
         for (let i = 0; i < 1000; ++i) {
-            record = randomRecord()
+            let record: Record = randomRecord()
             expect(record.High).to.be.at.least(record.Open)
             expect(record.High).to.be.at.least(record.Low)
             expect(record.High).to.be.at.least(record.Close)
